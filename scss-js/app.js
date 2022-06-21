5433f9e1ba3de564f1f384a30834dcc5b0d125a2
sasmirno <sasmirno1@gmail.com>
2022-06-21 21:38:22 +0300

    Добавил scss и js исходники

---
+++ scss-js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as burger from "./modules/burger.js";
+import * as accordion from "./modules/accordion.js";
+
+flsFunctions.isWebp();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
