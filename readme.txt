7241ce7e1867148e0471a37b4d03e6ae9b671b7a
sasmirno <sasmirno1@gmail.com>
2022-07-06 22:02:32 +0300

    Ещё одна правка багов

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-Лэндинг полностью выполнен на относительных единицах измерения, с исползьзованием Gulp, scss, flexbox и чутка drid.
+Лэндинг полностью выполнен на относительных единицах измерения, с исползьзованием Gulp, scss, flexbox, drid и чутка js.
 
 Макет
 https://www.figma.com/file/6DfnHqcXvrOOuIw5yPnW2C/Interno?node-id=0%3A1
--- scss-js/modules/burger.js
+++ scss-js/modules/burger.js
@@ -1,7 +1,9 @@
 //burger
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
+let menuBtn = document.querySelector('.burger__menu-btn');
+let menu = document.querySelector('.burger__menu');
+let back = document.querySelector('body');
 menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+	menuBtn.classList.toggle('burger_active');
+	menu.classList.toggle('burger_active');
+	back.classList.toggle('burger_lock');
 })
